14f716f2fa671c098af478f3747ff0917a0c9d8d
hasungpil <dependspic@gmail.com>
2024-03-20 13:01:18 +0900

    fix : 첨부파일 수정

--- src/components/Noimage.jsx
+++ src/components/Noimage.jsx
@@ -10,7 +10,11 @@ const Noimage = ({ props, className }) => {
       }
     >
       {props.image && (
-        <img src={props.image} alt="" className="absolute inset-0 w-full h-full object-cover " />
+        <img
+          src={URL.createObjectURL(props.image)} //이미지 미리보기 캐시로 저장됨. 데이터화 된것이 아님..
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover "
+        />
       )}
       <div>
         <PiImageLight />
