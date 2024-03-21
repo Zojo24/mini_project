e53af6c26231334ab25005f389b1cc053bd8031d
Zojo <hchoi2894@gmail.com>
2024-03-21 18:21:59 +0900

    fix: 로그인/회원가입 로직 수정 (handler)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,28 @@ import { useNavigate } from "react-router-dom";
 import "../styles/pages/login.css";
 import axios from "axios";
 import { useLoginStore } from "../store/loginStore";
+import { useRegisterStore } from "../store/RegisterStore";
 import Toast from "../components/Toast";
 
 const Login = ({ close, ...props }) => {
+  const setLogin = useLoginStore((state) => state.setLogin);
+  const setRegister = useRegisterStore((state) => state.setRegister);
+
   const [isTab, setIsTab] = useState("login");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const [loginToast, setLoginToast] = useState(false);
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerError, setRegisterError] = useState("");
+  const [registerToast, setRegisterToast] = useState(false);
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+  const [birthMonth, setBirthMonth] = useState("");
+  const [birthDay, setBirthDay] = useState("");
+
   const navigate = useNavigate();
 
   const handleTab = (tab) => {
@@ -19,33 +37,53 @@ const Login = ({ close, ...props }) => {
     navigate("/findaccount");
   };
 
-  const setLogin = useLoginStore((state) => state.setLogin);
-  const setRegister = useRegisterStore((state) => state.setRegister);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState("");
-  const [registerError, setRegisterError] = useState("");
-  const [loginToast, setLoginToast] = useState(false);
-  const [registerToast, setRegisterToast] = useState(false);
-  const [name, setName] = useState("");
-  const [confirmPassword, setconfirmPassword] = useState("");
-  const [birthYear, setbirthYear] = useState("");
-  const [birthMonth, setbirthMonth] = useState("");
-  const [birthDay, setbirthDay] = useState("");
+  const handleLoginEmail = (value) => {
+    setLoginEmail(value);
+  };
 
+  const handleLoginPassword = (value) => {
+    setLoginPassword(value);
+  };
+  const handleName = (value) => {
+    setName(value);
+  };
+  const handleRegisterEmail = (value) => {
+    setRegisterEmail(value);
+  };
+
+  const handleBirthYear = (value) => {
+    setBirthYear(value);
+  };
+
+  const handleBirthMonth = (value) => {
+    setBirthMonth(value);
+  };
+
+  const handleBirthDay = (value) => {
+    setBirthDay(value);
+  };
+
+  const handleRegisterPassword = (value) => {
+    setRegisterPassword(value);
+  };
+
+  const handleConfirmPassword = (value) => {
+    setConfirmPassword(value);
+  };
+
+  // 로그인 로직
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post("/api/members/login", {
-        email,
-        password,
+        email: loginEmail,
+        password: loginPassword,
       });
 
       //header jwt 토큰 정보 받아오기
-      //TODO: 추후 헤더 이름 확인 필요
       let jwtToken = response.headers["Authorization"];
-
+      //TODO: 추후 헤더 이름 확인 필요
       if (response.data.success && jwtToken) {
         localStorage.setItem("token", jwtToken);
         setLogin(true);
@@ -71,10 +109,11 @@ const Login = ({ close, ...props }) => {
     }
   };
 
+  // 회원가입 로직
   const handleRegister = async (e) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
+    if (registerPassword !== confirmPassword) {
       setRegisterError("비밀번호가 일치하지 않습니다.");
       setRegisterToast(true);
       return;
@@ -82,15 +121,17 @@ const Login = ({ close, ...props }) => {
 
     const requestData = {
       name,
-      email,
+      email: registerEmail,
       dateOfBirth: `${birthYear}/${birthMonth}/${birthDay}`,
-      password,
+      password: registerPassword,
     };
     try {
+      //TODO: 추후 URL 확인 필요
       const response = await axios.post("/api/register", requestData);
-      navigate("/");
+      navigate("/login");
     } catch (error) {
       if (error.response) {
+        setRegister(true);
         switch (error.response.status) {
           default:
             setRegisterError("회원가입 중 예기치 않은 오류가 발생했습니다.");
@@ -100,6 +141,8 @@ const Login = ({ close, ...props }) => {
           "서버에 접속할 수 없습니다. 네트워크 상태를 확인해 주세요."
         );
       }
+      setRegisterToast(true);
+      console.log("Register failed", error);
     }
   };
   return (
@@ -132,10 +175,8 @@ const Login = ({ close, ...props }) => {
               <Input
                 type="email"
                 required
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
+                value={loginEmail}
+                onChange={handleLoginEmail}
               />
             </div>
             <div>
@@ -143,10 +184,8 @@ const Login = ({ close, ...props }) => {
               <Input
                 type="password"
                 required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
+                value={loginPassword}
+                onChange={handleLoginPassword}
               />
             </div>
             <button type="submit" className="btn-blue xl2">
@@ -172,27 +211,63 @@ const Login = ({ close, ...props }) => {
           <form className="login-form" onSubmit={handleRegister}>
             <div>
               이름
-              <Input type="name" required />
+              <Input type="text" required value={name} onChange={handleName} />
             </div>
             <div>
               이메일
-              <Input type="email" required />
+              <Input
+                type="email"
+                required
+                value={registerEmail}
+                onChange={handleRegisterEmail}
+              />
             </div>
             <div>
               생년월일
               <div className="birth">
-                <Input type="number" placeholder="YYYY" required />년
-                <Input type="number" placeholder="MM" required />월
-                <Input type="number" placeholder="DD" required />일
+                <Input
+                  type="number"
+                  placeholder="YYYY"
+                  required
+                  value={birthYear}
+                  onChange={handleBirthYear}
+                />
+                년
+                <Input
+                  type="number"
+                  placeholder="MM"
+                  required
+                  value={birthMonth}
+                  onChange={handleBirthMonth}
+                />
+                월
+                <Input
+                  type="number"
+                  placeholder="DD"
+                  required
+                  value={birthDay}
+                  onChange={handleBirthDay}
+                />
+                일
               </div>
             </div>
             <div>
               비밀번호
-              <Input type="password" required />
+              <Input
+                type="password"
+                required
+                value={registerPassword}
+                onChange={handleRegisterPassword}
+              />
             </div>
             <div>
               비밀번호 확인
-              <Input type="password" required />
+              <Input
+                type="password"
+                required
+                value={confirmPassword}
+                onChange={handleConfirmPassword}
+              />
             </div>
             <button type="submit" className="btn-blue xl2">
               Sign Up
@@ -203,7 +278,7 @@ const Login = ({ close, ...props }) => {
             onOpen={registerToast}
             onClose={() => setRegisterToast(false)}
           >
-            {loginError}
+            {registerError}
           </Toast>
         </>
       )}
